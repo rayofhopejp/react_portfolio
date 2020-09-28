1aefd55fb10472ad42382b3791916b9bd2fa453c
NaomiatLibrary <ray.oshikawa@gmail.com>
2020-09-28 20:31:35 +0900

    add productrow:hover

--- react_portfolio/src/Contacts.js
+++ react_portfolio/src/Contacts.js
@@ -9,8 +9,8 @@ class ContactsContents extends React.Component {
         contacts:[
             {name:"Google Scholar",link:"https://scholar.google.com/citations?user=Hc0gTU0AAAAJ"},
             {name:"ResearchGate",link:"https://www.researchgate.net/profile/Ray_Oshikawa"},
-          　{name:"Twitter",link:"https://twitter.com/NaomiatLibrary",icon:TwitterIcon},
-            {name:"GitHub",link:"https://github.com/NaomiatLibrary",icon:"https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"},
+          　{name:"Twitter",link:"https://twitter.com/NaomiatLibrary"},
+            {name:"GitHub",link:"https://github.com/NaomiatLibrary"},
             {name:"Blog",link:"https://naomi-notebook.hatenablog.com/"},
             
         ]
